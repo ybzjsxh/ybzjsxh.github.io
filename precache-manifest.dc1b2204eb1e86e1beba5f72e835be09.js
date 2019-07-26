ef66805e6810101fce248edbf309ddbdad3c3d16
ybzjsxh <musiq841@gmail.com>
2019-07-26 14:44:44 +0800

    separate code and static pages

---
+++ precache-manifest.dc1b2204eb1e86e1beba5f72e835be09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "6eba4944a9344c4804bb",
+    "url": "./static/js/main.6eba4944.chunk.js"
+  },
+  {
+    "revision": "d5fd38c5fa2b6fd4be8d",
+    "url": "./static/js/2.d5fd38c5.chunk.js"
+  },
+  {
+    "revision": "6eba4944a9344c4804bb",
+    "url": "./static/css/main.e9b53b49.chunk.css"
+  },
+  {
+    "revision": "d5fd38c5fa2b6fd4be8d",
+    "url": "./static/css/2.faad1c27.chunk.css"
+  },
+  {
+    "revision": "cb2076ae96faa023bf23da75083f3d74",
+    "url": "./index.html"
+  }
+];
